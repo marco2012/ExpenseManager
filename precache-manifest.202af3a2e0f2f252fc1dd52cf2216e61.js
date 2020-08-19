4f35c445c12c131159457c2ab075ae08e96cd9f7
marco2012 <marco.fox.marco@gmail.com>
2020-08-19 12:35:25 +0200

    Updates

---
+++ precache-manifest.202af3a2e0f2f252fc1dd52cf2216e61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599440318cbf12acefb6f4942722c112",
+    "url": "/marco2012/ExpenseManager/index.html"
+  },
+  {
+    "revision": "ce70a8a01dd2f87a4ad7",
+    "url": "/marco2012/ExpenseManager/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "76ed10d58b5f55a329f8",
+    "url": "/marco2012/ExpenseManager/static/css/main.ffc1973c.chunk.css"
+  },
+  {
+    "revision": "ce70a8a01dd2f87a4ad7",
+    "url": "/marco2012/ExpenseManager/static/js/2.ae678821.chunk.js"
+  },
+  {
+    "revision": "74f4e1eda03d9ac324f4b3574b48fc97",
+    "url": "/marco2012/ExpenseManager/static/js/2.ae678821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76ed10d58b5f55a329f8",
+    "url": "/marco2012/ExpenseManager/static/js/main.321207a1.chunk.js"
+  },
+  {
+    "revision": "c023162f4565b4c59d88",
+    "url": "/marco2012/ExpenseManager/static/js/runtime-main.48eb21ac.js"
+  }
+]);
